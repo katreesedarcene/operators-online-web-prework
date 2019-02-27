183c3bd21b0fce9eef575936d1f0879a14297ca0
Kat B <katreesebarnes@gmail.com>
2019-02-27 20:43:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -13,14 +13,8 @@ end
 
 def not_safe?(speed)
   
-   speed > 60 && speed <40
-    true
-    elsif speed < 40
-    true
-  elsif  speed < 40  &&  speed > 60 
-  
-  false
-  
+   speed > 60 && speed < 40 ? true : false
+    
 	end
 end
 	
